6635dbb360e8be4110e5b3bcc349056e36c5e3be
Marianna <Marianna@Marianna-PC>
2018-05-14 13:57:58 +0200

    Second

--- java_matrix/src/java_matrix/Matrix.java
+++ java_matrix/src/java_matrix/Matrix.java
@@ -1,39 +1,59 @@
 package java_matrix;
 
 public class Matrix {
-
-	public static void main(String[] args) {
-		
-		int szamlalo = 0;
-		
-		String[][] matrixEgy = new String[5][10];
+	
+	int[][] matrixEgy = new int[4][6];
 		
 		
-		// a mátrix feltöltése
-		for( int i = 0; i < matrixEgy.length; i++) {
-			for( int y = 0; y < matrixEgy[i].length; y++) {
-				matrixEgy[i][y] = ""+szamlalo;
-				szamlalo++;
-			};
+// a mátrix feltöltése 1-től folyamatosan
+public Matrix() {	
+	int szamlalo = 1;
+	for( int i = 0; i < this.matrixEgy.length; i++) {
+		for( int y = 0; y < this.matrixEgy[i].length; y++) {
+			this.matrixEgy[i][y] = szamlalo;
+			szamlalo++;
 		};
-
-		
-		// a mátrix megfordítása
-		String[] matrixEgyResz = new String[matrixEgy.length];
-		for( int i = 0; i < (matrixEgy.length); i++) {
-			matrixEgyResz[i] = matrixEgy[i][0];
-		};
-		
-		
-		// a mátrix kiíratása
-		for(int i = 0; i < matrixEgy.length; i++) {
-			for( int y = 0; y < matrixEgy[i].length; y++ ) {
-				System.out.print(matrixEgy[i][y] + " " + matrixEgyResz[i] + ";  ");
-			}
-			System.out.println();
-		};
-
-		
-	}
-
+	};
 }
+
+// a mátrix feltöltése a megadott értékkel
+public Matrix(int konstans) {	
+	for( int i = 0; i < this.matrixEgy.length; i++) {
+		for( int y = 0; y < this.matrixEgy[i].length; y++) {
+			this.matrixEgy[i][y] = konstans;
+		};
+	};
+}
+
+// a mátrix feltöltése az adott kezdő értéktől adott lépéssel
+public Matrix(int startValue, int delta) {	
+	int szamlalo = 0;
+	for( int i = 0; i < this.matrixEgy.length; i++) {
+		for( int y = 0; y < this.matrixEgy[i].length; y++) {
+			this.matrixEgy[i][y] = startValue + szamlalo;
+			szamlalo = szamlalo + delta;
+		};
+	};
+}
+
+// a mátrix kiíratása
+public void MatrixPrint() {
+	for(int i = 0; i < this.matrixEgy.length; i++) {
+		for( int y = 0; y < this.matrixEgy[i].length; y++ ) {
+			System.out.printf("%4d", this.matrixEgy[i][y]);
+		};
+		System.out.println("");
+	};
+}
+		
+// a mátrix fordított kiírása
+public void MatrixMirrorPrint() {
+		for(int i = this.matrixEgy.length-1; i >= 0; i--) {
+			for(int y = this.matrixEgy[i].length-1; y >= 0; y--) {
+				System.out.printf("%4d",this.matrixEgy[i][y]);
+			};
+			System.out.println();;
+		};
+}
+
+}	
---
+++ java_matrix/src/java_matrix/Matrix_Main.java
@@ -0,0 +1,17 @@
+package java_matrix;
+
+public class Matrix_Main {
+
+	public static void main(String[] args) {
+
+		Matrix myMatrix = new Matrix();
+		
+		myMatrix.MatrixPrint();
+		
+		System.out.println("-----------------------");
+		
+		myMatrix.MatrixMirrorPrint();
+
+	}
+
+}
